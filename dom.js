299297382685540e6d2eee25066585b6ee391ce6
raghavendrasangam <raghavendrasangam0007@gmail.com>
2023-07-06 00:04:12 +0530

    domfile added

---
+++ dom.js
@@ -0,0 +1,22 @@
+// console.dir(document);
+
+// console.log(document.domain);
+
+// console.log(document.URL);
+
+// console.log(document.title);
+// console.log('hi');
+// document.all[10].textContent = 'Hello';
+var headerTitle = document.getElementById('header-title');
+// console.log(headerTitle);
+
+headerTitle.style.border='solid 3px #000';
+
+var titleadd = document.getElementById('titleid');
+// console.log(titleadd);
+titleadd.style.fontWeight = 'bold';
+titleadd.style.color= 'green';
+
+var titleclass = document.getElementsByClassName('list-group-item');
+// console.log(titleclass);
+titleclass[1].style.backgroundColor = 'lightpink';
